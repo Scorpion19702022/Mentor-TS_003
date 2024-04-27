ed56ebd748daf8314bc1db9115c96d9c55e3a2ec
skorpion <jmosinski@o2.pl>
2024-04-27 13:22:21 +0200

    test

---
+++ src/Components/Inputs.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from './Inputs.module.css'
+
+const Inputs = () => {
+	return (
+		<div className={styles.box_inputs}>
+			<p>inputy</p>
+		</div>
+	)
+}
+
+export default Inputs
