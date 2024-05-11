10653d1587df45e91ef92283d613955b61466274
skorpion <jmosinski@o2.pl>
2024-05-11 21:04:41 +0200

    test

--- src/Components/Result.tsx
+++ src/Components/Result.tsx
@@ -10,8 +10,15 @@ const Result = () => {
 		return (
 			<div className={styles.box_result} key={toDo.id}>
 				<h3 className={toDo.isComplete ? styles.heading_out : styles.heading_normal}>{toDo.task}</h3>
-				<button onClick={() => deleteToDo(toDo.id)}>Delete</button>
-				<input type='checkbox' checked={toDo.isComplete} onClick={() => completeToDo(toDo.id)} />
+				<button className={styles.btn_delete} onClick={() => deleteToDo(toDo.id)}>
+					Delete
+				</button>
+				<input
+					className={styles.check_box}
+					type='checkbox'
+					checked={toDo.isComplete}
+					onClick={() => completeToDo(toDo.id)}
+				/>
 			</div>
 		)
 	})
